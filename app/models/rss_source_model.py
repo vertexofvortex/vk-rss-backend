610cd1ba1a4f248db045461cccb9fc2605cb02c0
vertexofvortex <tbm92ru@gmail.com>
2023-04-18 14:04:32 +0300

    🐛 Added unique constraints to source model

--- app/models/rss_source_model.py
+++ app/models/rss_source_model.py
@@ -9,9 +9,9 @@ class RSSSourceModel(Base):
     __tablename__ = "rss_sources"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
+    title = Column(String, nullable=False, unique=True)
     description = Column(String, nullable=False)
-    rss_url = Column(String, nullable=False)
+    rss_url = Column(String, nullable=False, unique=True)
     logo = Column(Integer, ForeignKey("rss_sources_logos.id"), nullable=True)
     
     posts = relationship("RSSPostModel", back_populates="source")
