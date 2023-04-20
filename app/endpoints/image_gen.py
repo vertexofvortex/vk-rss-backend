48d404f244d584a480c96606ebd6368fe50b1e90
vertexofvortex <tbm92ru@gmail.com>
2023-04-20 18:20:22 +0300

    ♻️ Posting refactor

--- app/endpoints/image_gen.py
+++ app/endpoints/image_gen.py
@@ -1,35 +1,60 @@
 import base64
+import traceback
 from io import BytesIO
-from typing import Annotated
+from typing import Annotated, Union
+
 import aiohttp
 from fastapi import APIRouter, Depends, File, Form, HTTPException, Response, UploadFile
+
 from app.schemas.generated_image import GeneratedImageCreate
 from app.security import auth
-
 from app.utils.img_gen.img_gen import generate_image
-
 
 router = APIRouter(tags=["Image generating"])
 
 
-@router.post("/check_image_gen")
-async def check_image_gen(
+@router.post("/generate_snippet")
+async def generate_snippet(
     auth: Annotated[bool, Depends(auth)],
     title: str = Form(),
     description: str = Form(),
     source: str = Form(),
-    image: UploadFile = File(),
+    image: Union[UploadFile, None] = File(None),
+    image_url: Union[str, None] = Form(None),
     logo: UploadFile = File(),
 ):
-    generated_image = generate_image(
-        title=title,
-        description=description,
-        source=source,
-        image_bytes=BytesIO(image.file.read()),
-        logo_bytes=BytesIO(logo.file.read()),
-    )
+    if image_url is None:
+        generated_image = generate_image(
+            title=title,
+            description=description,
+            source=source,
+            image_bytes=BytesIO(image.file.read()),
+            logo_bytes=BytesIO(logo.file.read()),
+        )
 
-    return Response(
-        content=generated_image.getvalue(),
-        media_type="image/png",
-    )
+        return Response(
+            content=generated_image.getvalue(),
+            media_type="image/png",
+        )
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(image_url) as response:
+                external_image = await response.read()
+
+        generated_image = generate_image(
+            title=title,
+            description=description,
+            source=source,
+            image_bytes=BytesIO(external_image),
+            logo_bytes=BytesIO(logo.file.read()),
+        )
+
+        return Response(
+            content=generated_image.getvalue(),
+            media_type="image/png",
+        )
+    except:
+        raise HTTPException(
+            status_code=500, detail="An error occured in image generation."
+        )
--- app/endpoints/vk_api.py
+++ app/endpoints/vk_api.py
@@ -1,15 +1,16 @@
 from io import BytesIO
 from typing import Annotated
+
 from fastapi import APIRouter, Depends, File, Form, HTTPException, UploadFile
 from sqlalchemy.orm import Session
+
 from app.crud import CRUD, vk_usertoken_methods
 from app.depends import get_db
 from app.schemas.vk_group_schema import VKGroupExternal
 from app.security import auth
+from app.utils.aes_tools.aes_cipher import aes_tools
 from app.utils.img_gen.img_gen import generate_image
 from app.utils.vk_api_wrapper.vk_api_wrapper import VKAPIWrapper
-from app.utils.aes_tools.aes_cipher import aes_tools
-
 
 router = APIRouter(tags=["VK API"])
 
@@ -56,15 +57,63 @@ async def get_group_by_id(
     return group
 
 
+# @router.post("/vk_api/post")
+# async def create_post(
+#     auth: Annotated[bool, Depends(auth)],
+#     title: str = Form(),
+#     description: str = Form(),
+#     source: str = Form(),
+#     source_url: str = Form(),
+#     image: UploadFile = File(),
+#     logo: UploadFile = Form(),
+#     usertoken_id: int = Form(),
+#     passphrase: str = Form(),
+#     group_id: int = Form(),
+#     db: Session = Depends(get_db),
+# ):
+#     enc_token = vk_usertoken_methods.get_token_by_id(db, usertoken_id).token
+#     dec_token = aes_tools.decrypt(enc_token, passphrase)
+
+#     vk_api = VKAPIWrapper(dec_token)
+
+#     try:
+#         group_vk_id = CRUD.vk_group_methods.get_group_by_id(db, group_id).vk_id
+#     except:
+#         raise HTTPException(
+#             status_code=500, detail=f"Group with ID {group_id} does not exist."
+#         )
+
+#     image_bytes = BytesIO(image.file.read())
+#     logo_bytes = BytesIO(logo.file.read())
+#     message = f"{title}%0A%0A{description}%0A%0A{source_url}"
+
+#     generated_image = generate_image(
+#         title=title,
+#         description=description,
+#         source=source,
+#         image_bytes=image_bytes,
+#         logo_bytes=logo_bytes,
+#     )
+
+#     generated_image.seek(0)
+
+#     await vk_api.create_post(
+#         group_id=group_vk_id,
+#         message=message,
+#         copyright=source_url,
+#         image=generated_image,
+#         image_filename="image.png",
+#     )
+
+
 @router.post("/vk_api/post")
-async def create_post(
+async def post(
     auth: Annotated[bool, Depends(auth)],
     title: str = Form(),
     description: str = Form(),
     source: str = Form(),
     source_url: str = Form(),
     image: UploadFile = File(),
-    logo: UploadFile = Form(),
     usertoken_id: int = Form(),
     passphrase: str = Form(),
     group_id: int = Form(),
@@ -82,24 +131,13 @@ async def create_post(
             status_code=500, detail=f"Group with ID {group_id} does not exist."
         )
 
-    image_bytes = BytesIO(image.file.read())
-    logo_bytes = BytesIO(logo.file.read())
+    image_bytes = image.file.read()
     message = f"{title}%0A%0A{description}%0A%0A{source_url}"
-
-    generated_image = generate_image(
-        title=title,
-        description=description,
-        source=source,
-        image_bytes=image_bytes,
-        logo_bytes=logo_bytes,
-    )
-
-    generated_image.seek(0)
 
     await vk_api.create_post(
         group_id=group_vk_id,
         message=message,
         copyright=source_url,
-        image=generated_image,
+        image=image_bytes,
         image_filename="image.png",
     )
